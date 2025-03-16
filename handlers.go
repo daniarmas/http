4394561196f866f7eef7ebc3bc33a5b76cccf7c4
daniarmas <daniel.armas9706@gmail.com>
2025-03-16 18:01:30 -0400

    Add the handlers files to contain the server handlers

---
+++ handlers.go
@@ -0,0 +1,14 @@
+package http
+
+import "net/http"
+
+// HealthCheckResponse represents the structure of the health check response
+type HealthCheckResponse struct {
+	Status string `json:"status"`
+}
+
+// HealthCheckHandler handles HTTP requests for checking the health status of the server.
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	res := HealthCheckResponse{Status: "healthy"}
+	OK(w, r, res)
+}
